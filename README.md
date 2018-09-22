1153f00519833b6613ffdab2b71011be97d5beae
Alessio Enrico Repetti <repeale@gmail.com>
2018-09-22 23:50:29 +0200

    minimal react webpack babel setup

---
+++ README.md
@@ -0,0 +1,16 @@
+# my-stack
+
+## Features
+
+* React 16
+* Webpack 4
+* Babel 7
+* Hot Module Replacement
+
+## Installation
+
+* `git clone git@github.com:my-stack.git`
+* cd my-stack
+* npm install
+* npm start
+* visit `http://localhost:8080/`
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack')
+const path = require('path')
+
+// --- plugins
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var WebpackCleanupPlugin = require('clean-webpack-plugin')
+
+// --- folders
+const FOLDER = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src')
+}
+
+const webpackConfig = {
+  entry: {
+    app: ['./src/index.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: FOLDER.SRC,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    filename: '[name].[hash].bundle.js',
+    path: FOLDER.DIST,
+    publicPath: '/',
+    pathinfo: false
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new WebpackCleanupPlugin([FOLDER.DIST]),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      hash: false,
+      inject: true
+    })
+  ],
+  devServer: {
+    contentBase: FOLDER.DIST,
+    hot: true
+  }
+}
+
+module.exports = webpackConfig
