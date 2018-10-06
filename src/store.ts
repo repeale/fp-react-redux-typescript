f5cbcd0ac18886fbfbbd4da7ab0a36f6766ce1c0
Alessio Enrico Repetti <repeale@gmail.com>
2018-10-06 22:01:46 +0200

    chore: added redux-devtools-extension in development mode

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,13 @@
 import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/root'
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly'
 
 export default function configureStore(initialState = {}) {
-  return createStore(rootReducer, applyMiddleware(thunk, classDispatcher))
+  return createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk, classDispatcher))
+  )
 }
 
 const classDispatcher = ({dispatch, getState}) => next => action => {
