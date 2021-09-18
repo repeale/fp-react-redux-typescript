cc222d53a5a55c86d0f907470f328cd584bff49b
Alessio Enrico Repetti <repeale@gmail.com>
2021-09-18 22:22:00 +0200

    chore: rename path

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {configureStore} from './store'
-import {App} from './components/App'
+import {App} from './components/app'
 
 ReactDOM.render(
   <Provider store={configureStore()}>
